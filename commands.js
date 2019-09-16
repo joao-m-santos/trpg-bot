2b1890a2a9c761fedb273c91658c33940766d217
joao-m-santos <jm_santos7@hotmail.com>
2019-09-16 14:09:44 +0100

    Removed commands.js and players.json

--- commands.js
+++
@@ -1,7 +0,0 @@
-const sheetFunction = require("./commands/characterSheet");
-const playerFunction = require("./commands/player");
-
-module.exports = {
-    sheets: (action, message) => sheetFunction(action, message),
-    player: (action, message) => playerFunction(action, message)
-};
